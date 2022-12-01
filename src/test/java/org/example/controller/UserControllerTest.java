5bcd826663310d066d03976bb1f526cbd16919ff
Nicola MARNAT <nicola.marnat@gmail.com>
2022-12-01 16:47:06 +0100

    Add test method for getAllUsers and getUserById

--- src/test/java/org/example/controller/UserControllerTest.java
+++ src/test/java/org/example/controller/UserControllerTest.java
@@ -16,6 +16,10 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 @ExtendWith(MockitoExtension.class)
 @WebMvcTest(value = UserController.class)
 class UserControllerTest {
@@ -42,15 +46,51 @@ class UserControllerTest {
 
         String expected = "{\"id\":1,\"firstName\":\"Nicola\",\"lastName\":\"Marnat\"}";
 
-        JSONAssert.assertEquals(expected, result.getResponse()
-                .getContentAsString(), false);
+        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
     }
 
     @Test
-    void testGetAllUsers() {
+    void testGetAllUsers() throws Exception {
+        User user1 = new User();
+        user1.setId(1);
+        user1.setFirstName("Azerty");
+        user1.setLastName("Uiop");
+
+        User user2 = new User();
+        user2.setId(2);
+        user2.setFirstName("Qsdfgh");
+        user2.setLastName("Jklm");
+
+        List<User> users = Arrays.asList(user1, user2);
+
+        Mockito.when(userService.findAll()).thenReturn(users);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/users")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        String expected = "[{\"id\":1,\"firstName\":\"Azerty\",\"lastName\":\"Uiop\"},{\"id\":2,\"firstName\":\"Qsdfgh\",\"lastName\":\"Jklm\"}]";
+
+        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
     }
 
     @Test
-    void testGetUserById() {
+    void testGetUserById() throws Exception {
+        User user = new User();
+        user.setId(1);
+        user.setFirstName("Azerty");
+        user.setLastName("Uiop");
+
+        Mockito.when(userService.findById(1)).thenReturn(Optional.of(user));
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/user?id=1")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        String expected = "{\"id\":1,\"firstName\":\"Azerty\",\"lastName\":\"Uiop\"}";
+
+        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
     }
 }
