f301628c050ee05faf00ec662e228ed6c659c761
Nicola MARNAT <nicola.marnat@gmail.com>
2022-12-01 14:10:19 +0100

    Switch User Controller to RestController

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -3,12 +3,12 @@ package org.example.controller;
 import org.example.model.User;
 import org.example.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
     @Autowired
     private UserRepository userRepository;
