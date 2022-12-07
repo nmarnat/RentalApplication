d6ce617ba2fc25005e6c5e4bd752a84494f559fc
Nicola MARNAT <nicola.marnat@gmail.com>
2022-12-07 18:14:56 +0100

    Add method to validate Rental, throws InvalidRentalException

--- src/main/java/org/example/service/RentalServiceImpl.java
+++ src/main/java/org/example/service/RentalServiceImpl.java
@@ -1,12 +1,14 @@
 package org.example.service;
 
 import org.example.exception.InvalidRentalException;
+import org.example.model.Book;
 import org.example.model.Rental;
 import org.example.model.User;
 import org.example.repository.RentalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,22 +17,19 @@ public class RentalServiceImpl implements RentalService {
     private final RentalRepository rentalRepository;
 
     private final UserService userService;
+    private final BookService bookService;
 
     @Autowired
-    public RentalServiceImpl(RentalRepository rentalRepository, UserService userService) {
+    public RentalServiceImpl(RentalRepository rentalRepository, UserService userService, BookService bookService) {
         this.rentalRepository = rentalRepository;
         this.userService = userService;
+        this.bookService = bookService;
     }
 
     @Override
     public Rental save(Rental rental) throws InvalidRentalException {
-        Optional<User> user = userService.findById(rental.getUser().getId());
-        if (user.isPresent() && user.get().getRental() != null) {
-            throw new InvalidRentalException("User already has a rental");
-        }
-        else {
-            return rentalRepository.save(rental);
-        }
+        validate(rental);
+        return rentalRepository.save(rental);
     }
 
     @Override
@@ -58,4 +57,26 @@ public class RentalServiceImpl implements RentalService {
         rentalRepository.deleteById(id);
     }
 
+    public void validate(Rental rental) throws InvalidRentalException {
+        Optional<User> user = userService.findById(rental.getUser().getId());
+        if (user.isEmpty()) {
+            throw new InvalidRentalException("User does not exist");
+        }
+        if (user.get().getRental() != null) {
+            throw new InvalidRentalException("User already has a rental");
+        }
+
+        List<Book> books = rental.getBooks();
+        for (Book b : books) {
+            Optional<Book> book = bookService.findById(b.getId());
+            if (book.isEmpty()) {
+                throw new InvalidRentalException("Book " + b.getName() + " does not exist");
+            }
+            if (book.get().getRental() != null) {
+                throw new InvalidRentalException("Book " + b.getName() + " is already rented");
+            } else {
+                book.get().setRental(rental);
+            }
+        };
+    }
 }
